5b876baf3afa90c69a18a4b2e73d5e0175b83de4
Taha Khan <tahakhan@Tahas-MBP.fios-router.home>
2021-01-03 18:20:36 -0500

    added user input, list, functionality to add songs to list in created playlist

--- spotifyPlaylist.py
+++ spotifyPlaylist.py
@@ -19,3 +19,22 @@ playlist_description = input("Enter a playlist description: ")
 
 
 spotifyObject.user_playlist_create(user = username, name=playlist_name,public=True, description=playlist_description)
+
+
+user_input = input('Enter the song:')
+list_of_songs = []
+
+while user_input != 'quit':
+    result = spotifyObject.search(q=user_input)
+    #print(json.dumps(result,sort_keys=4,indent=4))
+    list_of_songs.append(result['tracks']['items'][0]['uri'])
+    user_input = input('Enter the song: ')
+
+#find playlist
+prePlaylist = spotifyObject.user_playlists(user=username)
+playlist = prePlaylist['items'][0]['id']
+
+#add songs
+
+spotifyObject.playlist_add_items(playlist_id=playlist, items=list_of_songs)
+
